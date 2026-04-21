d42208e9f1df2a92027bfa1525005e2429240b21
Roshni Giri <124186406+Roshnigiri99@users.noreply.github.com>
2026-04-21 19:00:13 +0530

    Update sentiment_analysis.py

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,15 +1,24 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import string
 
 analyzer = SentimentIntensityAnalyzer()
 
+
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text.strip()
+
+
 def analyze_sentiment(text):
+    text = clean_text(text)
 
     score = analyzer.polarity_scores(text)["compound"]
 
-    if score > 0.05:
+    if score >= 0.05:
         return "Positive"
 
-    elif score < -0.05:
+    elif score <= -0.05:
         return "Negative"
 
     else:
